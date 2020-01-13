12521c0e6d6489fba15e59cead86b0b099fd3fb5
Alex2Pena <attn.Alex.P@gmail.com>
2020-01-13 13:46:22 -0800

    Created HTML pages & app.js page.

---
+++ app.js
@@ -0,0 +1,12 @@
+var hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm']
+var loction  = ['Seattle','Tokyp','Dubai','Paris','Lima']
+
+seattleStore = {
+    customerMin: , 
+    customerMax: ,
+    customerAverage: ,
+}
+
+for (var i=0; i<hours.length; i++){
+    
+}
